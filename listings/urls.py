f92462a411b81cca7816a8dad8b6e6e1ce5870cb
sunitsai <58470631+sunitsai@users.noreply.github.com>
2021-07-29 11:40:48 +0530

    Update urls.py

--- listings/urls.py
+++ listings/urls.py
@@ -4,3 +4,6 @@ urlpatterns = [
     path("booking/",BookingInfoView.as_view()),
     path("booking/<int:price>",BookingInfoView.as_view()),
 ]
+
+
+# Execute URL : 127.0.0.1:8000/booking/70?price=lessthan
